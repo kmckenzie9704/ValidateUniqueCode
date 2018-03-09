3fe6aab124b99dc5c27e5b112abfb84df803495f
Kenneth McKenzieddfsdf <kenneth.mckenzie@myacuity.coms>
2018-03-09 12:42:59 -0500

    a

--- ValidateUniqueCode/Controllers/ValidateUniqueCodeController.cs
+++ ValidateUniqueCode/Controllers/ValidateUniqueCodeController.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using ValidateUniqueCode.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Newtonsoft.Json.Converters;
+//using Newtonsoft.Json.Serialization;
+//using Newtonsoft.Json.Converters;
 
 
 namespace ValidateUniqueCode.Controllers
